a04043f6de574d058f41c64439586a3904a29613
Arjun Chakravarthy Nunugonda <arjunchakri@gmail.com>
2026-01-02 17:51:46 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Define container names
+C_PORTAINER="portainer"
+C_NEXTCLOUD="nextcloud"
+C_WEBTOP="webtop"
+C_OPENWEBUI="open-webui"
+
+echo "--- 1. Creating Docker Volumes ---"
+docker volume create portainer_data
+docker volume create nextcloud_data
+docker volume create webtop_data
+docker volume create open-webui_data
+
+echo "--- 2. Cleaning up old containers (if they exist) ---"
+docker rm -f $C_PORTAINER $C_NEXTCLOUD $C_WEBTOP $C_OPENWEBUI 2>/dev/null || true
+
+echo "--- 3. Starting Portainer ---"
+# Accessible at https://<IP>:9443
+docker run -d \
+  --name $C_PORTAINER \
+  --restart=always \
+  -p 9443:9443 \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data \
+  portainer/portainer-ce:latest
+
+echo "--- 4. Starting Nextcloud ---"
+# Accessible at http://<IP>:8080
+docker run -d \
+  --name $C_NEXTCLOUD \
+  --restart=always \
+  -p 8080:80 \
+  -v nextcloud_data:/var/www/html \
+  -e SQLITE_DATABASE=nextcloud \
+  nextcloud:latest
+
+echo "--- 5. Starting Webtop (Ubuntu XFCE) ---"
+# Accessible at http://<IP>:3000
+docker run -d \
+  --name $C_WEBTOP \
+  --restart=unless-stopped \
+  -p 3000:3000 \
+  --security-opt seccomp=unconfined \
+  --shm-size="1gb" \
+  -e PUID=$(id -u) \
+  -e PGID=$(id -g) \
+  -e TZ=Etc/UTC \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v webtop_data:/config \
+  lscr.io/linuxserver/webtop:ubuntu-xfce
+
+echo "--- 6. Starting Open WebUI (for Ollama) ---"
+# Accessible at http://<IP>:3001
+# Note: --add-host allows the container to talk to Ollama running on the host machine
+docker run -d \
+  --name $C_OPENWEBUI \
+  --restart=always \
+  -p 3001:8080 \
+  --add-host=host.docker.internal:host-gateway \
+  -e OLLAMA_BASE_URL=http://host.docker.internal:11434 \
+  -v open-webui_data:/app/backend/data \
+  ghcr.io/open-webui/open-webui:main
+
+echo "--- All Containers Started ---"
+docker ps
